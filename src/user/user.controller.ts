6efad55ed0ba7e95a26ace62cf7a9efe2c5b9875
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-03-23 00:34:29 +0100

    feat: add user & crud user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common'
+import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger'
 
+import { CreateUserDto } from './dto'
+import { UserService } from './user.service'
+
+@ApiTags('user')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @ApiOkResponse({ description: 'User created successfully' })
+  createUser(@Body() dto: CreateUserDto) {
+    return this.userService.save(dto)
+  }
+
+  @Get(':idOrEmail')
+  @ApiOkResponse({ status: 200, description: '' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  findOneUser(@Param('idOrEmail') idOrEmail: string) {
+    return this.userService.findOne(idOrEmail)
+  }
+
+  @Delete(':id')
+  @ApiResponse({ status: 200, description: 'User successfully deleted' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  deleteUser(@Param('id', ParseUUIDPipe) id: string) {
+    return this.userService.remove(id)
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+
+import { User } from '@prisma/client'
+import { PrismaService } from '@prisma/prisma.service'
+import { genSaltSync, hashSync } from 'bcrypt'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  save(user: Partial<User>) {
+    // const hashedPassword = this.hashPassword(user.password)
+    const hashedPassword = user.password ? this.hashPassword(user.password) : undefined
+    if (!hashedPassword) {
+      throw new Error('Password is required')
+    }
+    return this.prismaService.user.create({
+      data: { name: user.name || '', email: user.email || '', password: hashedPassword, roles: ['USER'] }
+    })
+  }
+
+  findOne(idOrEmail: string) {
+    return this.prismaService.user.findFirst({ where: { OR: [{ id: idOrEmail }, { email: idOrEmail }] } })
+  }
+
+  remove(id: string) {
+    return this.prismaService.user.delete({ where: { id } })
+  }
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10))
+  }
+}
