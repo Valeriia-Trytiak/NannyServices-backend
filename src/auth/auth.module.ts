33ec68097a4db963358355ff6872a5bc0f4c560e
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-03-25 17:08:50 +0100

    feat: add jwt-strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,10 +7,11 @@ import { UserModule } from '@user/user.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { options } from './config'
+import { JwtStrategy } from './strategies'
 
 @Module({
   imports: [PassportModule, JwtModule.registerAsync(options()), UserModule],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
