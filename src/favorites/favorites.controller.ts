8bb48d45a27e1e84162a84f620c22234e09782ee
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-04-06 07:15:00 +0200

    fix: update favorites service. Add search by id and moved the repeated code into private methods¨

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common'
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
 
 import { JwtPayload } from '@auth/interfaces'
 import { Nanny } from '@prisma/client'
@@ -15,12 +15,21 @@ export class FavoritesController {
 
   @Get()
   @ApiOkResponse()
+  @ApiNotFoundResponse({ status: 404, description: 'User not found' })
   async getAll(@CurrentUser() user: JwtPayload): Promise<Nanny[]> {
     return this.favoritesService.getAll(user)
   }
 
+  @Get(':id')
+  @ApiOkResponse()
+  @ApiNotFoundResponse({ status: 404, description: 'Nanny not found' })
+  async getNannyByID(@Param('id', ParseIntPipe) id: number, @CurrentUser() user: JwtPayload): Promise<Nanny> {
+    return this.favoritesService.getById(id, user)
+  }
+
   @Post()
   @ApiOkResponse()
+  @ApiNotFoundResponse({ status: 404, description: 'Nanny or user not found' })
   async toggleFavorite(@Body() dataId: ToggleFavoriteDto, @CurrentUser() user: JwtPayload): Promise<Nanny[]> {
     return this.favoritesService.toggleFavorite(user, dataId.nannyId)
   }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -15,42 +15,52 @@ export class FavoritesService {
   ) {}
 
   async getAll(user: JwtPayload): Promise<Nanny[]> {
-    const userId = user.id
-
-    const currentUser = await this.userService.findOne(userId)
-
-    if (!currentUser) {
-      throw new NotFoundException('User not found')
-    }
+    const currentUser = await this.getCurrentUser(user)
 
     const favorites = await this.prismaService.nannyUserFavorite.findMany({
-      where: { userId },
+      where: { userId: currentUser.id },
       include: { nanny: true }
     })
     return favorites.map(favorite => favorite.nanny)
   }
 
+  async getById(nannyId: number, user: JwtPayload): Promise<Nanny> {
+    await this.getCurrentUser(user)
+    const nanny = await this.findNannyById(nannyId)
+    return nanny
+  }
+
   async toggleFavorite(user: JwtPayload, nannyId: number): Promise<Nanny[]> {
-    const userId = user.id
+    const currentUser = await this.getCurrentUser(user)
+    await this.findNannyById(nannyId)
 
-    const currentUser = await this.userService.findOne(userId)
-
-    if (!currentUser) {
-      throw new NotFoundException('User not found')
-    }
-
-    const nanny = await this.nanniesService.findById(nannyId)
-    if (!nanny) {
-      throw new NotFoundException('Nanny not found')
-    }
-
-    const favorite = await this.prismaService.nannyUserFavorite.findFirst({ where: { userId, nannyId } })
+    const favorite = await this.prismaService.nannyUserFavorite.findFirst({
+      where: { userId: currentUser.id, nannyId }
+    })
 
     if (favorite) {
       await this.prismaService.nannyUserFavorite.delete({ where: { id: favorite.id } })
     } else {
-      await this.prismaService.nannyUserFavorite.create({ data: { nannyId, userId } })
+      await this.prismaService.nannyUserFavorite.create({ data: { nannyId, userId: currentUser.id } })
     }
     return await this.getAll(user)
+  }
+
+  private async getCurrentUser(user: JwtPayload) {
+    const currentUser = await this.userService.findOne(user.id)
+
+    if (!currentUser) {
+      throw new NotFoundException('User not found')
+    }
+    return currentUser
+  }
+
+  private async findNannyById(nannyId: number) {
+    const nanny = await this.nanniesService.findById(nannyId)
+
+    if (!nanny) {
+      throw new NotFoundException('Nanny not found')
+    }
+    return nanny
   }
 }
